d5a90e522627cbe55f909ba152ed9b7b9ee3428d
Camille <camille@Mac.lan>
2025-08-27 01:59:26 +0200

    section reminder

--- src/components/List/Card.jsx
+++ src/components/List/Card.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Card.css";
 
 const Card = ({ film }) => {
   return (
+    <Link to={`/films/${film.slug}`} className="card-link">
     <div className="card-container">
       <div className="card-image"><img src="/logo.svg" alt="Ciné Pocket" className="home-logo"/></div>
       <div className="card-infos">
@@ -10,6 +12,7 @@ const Card = ({ film }) => {
       <div className="card-description">{film.recommendations && film.recommendations[0]}</div>
       </div>
     </div>
+    </Link>
   );
 };
 
